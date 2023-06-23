120000a75f37681b3f4c0c739b60f08b0b296e8e
brunomaria71 <brunomaria71@yahoo.com>
2023-06-23 16:37:38 -0400

    made a variable to track errors

--- project3/index.js
+++ project3/index.js
@@ -34,60 +34,71 @@ const validateInputs = () => {
   const expiryValue = expiryInput.value.trim();
   const expyearValue = expyearInput.value.trim();
   const cvcValue = cvcInput.value.trim();
+  let hasErrors = false;
 
   if (cardholderNameValue === "") {
     setError(cardholderNameInput, "Can't be blank");
+    hasErrors = true;
   } else if (cardholderNameValue.length <= 2) {
     setError(cardholderNameInput, "Must be at least 2 letters");
+    hasErrors = true;
   } else {
     setSuccess(cardholderNameInput);
   }
 
   if (cardNumberValue === "") {
     setError(cardNumberInput, "Can't be blank");
+    hasErrors = true;
   } else if (/\D/.test(cardNumberValue)) {
     setError(cardNumberInput, "Wrong format, numbers only");
+    hasErrors = true;
   } else {
     setSuccess(cardNumberInput);
   }
 
   if (cvcValue === "") {
     setError(cvcInput, "Can't be blank");
+    hasErrors = true;
   } else if (cvcValue.length !== 3) {
     setError(cvcInput, "Must be 3 numbers");
+    hasErrors = true;
   } else if (/\D/.test(cvcValue)) {
     setError(cvcInput, "Wrong format, numbers only");
+    hasErrors = true;
   } else {
     setSuccess(cvcInput);
   }
 
   if (expiryValue === "") {
     setError(expiryInput, "Can't be blank");
+    hasErrors = true;
   } else if (/\D/.test(expiryValue)) {
     setError(expiryInput, "Wrong format, numbers only");
+    hasErrors = true;
   } else if (expiryValue.length !== 2) {
     setError(expiryInput, "Must be 2 numbers");
+    hasErrors = true;
   } else {
     setSuccess(expiryInput);
   }
 
   if (expyearValue === "") {
     setError(expyearInput, "Can't be blank");
+    hasErrors = true;
   } else if (/\D/.test(expyearValue)) {
     setError(expyearInput, "Wrong format, numbers only");
+    hasErrors = true;
   } else if (expyearValue.length !== 2) {
     setError(expyearInput, "Must be 2 numbers");
+    hasErrors = true;
   } else {
     setSuccess(expyearInput);
   }
 
-  // success message rendering
-  const errorElements = document.querySelectorAll(".error");
-  if (errorElements.length === 0) {
-    // All fields are valid, hide the form and display success message
+  if (!hasErrors) {
+    debugger;
     form.style.display = "none";
     const successMessage = document.querySelector(".thank-you-message");
-    // successMessage.innerText = "We've added your card details.";
     successMessage.style.display = "block";
   }
 };
