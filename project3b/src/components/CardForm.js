e635cb876634afd88a2988041ff7d69bc3246d1b
brunomaria71 <brunomaria71@yahoo.com>
2023-06-27 17:48:14 -0400

    form ip

--- project3b/src/components/CardForm.js
+++ project3b/src/components/CardForm.js
@@ -1,35 +1,38 @@
 // import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import "../styles.scss";
 
 const CardForm = () => {
   return (
-    <Form className="form">
-      <Form.Group className="mb-3" controlId="formCardholderName">
-        <Form.Label>CARDHOLDER NAME</Form.Label>
-        <Form.Control type="text" placeholder="Enter first and last name" />
-      </Form.Group>
+    <Container>
+      <Form className="form">
+        <Form.Group className="mb-3" controlId="formCardholderName">
+          <Form.Label>CARDHOLDER NAME</Form.Label>
+          <Form.Control type="text" placeholder="e.g. Jane Appleseed" />
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formCardholderName">
-        <Form.Label>CARD NUMBER</Form.Label>
-        <Form.Control type="text" placeholder="0000 0000 0000 0000" />
-      </Form.Group>
+        <Form.Group className="mb-3" controlId="formCardholderName">
+          <Form.Label>CARD NUMBER</Form.Label>
+          <Form.Control type="text" placeholder="e.g. 1234 5678 9123 0000" />
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formExpirationDate">
-        <Form.Label>EXPIRATION DATE (MM/YY)</Form.Label>
-        <Form.Control type="text" placeholder="MM" />
-        <Form.Control type="text" placeholder="YY" />
-      </Form.Group>
+        <Form.Group className="mb-3" controlId="formExpirationDate">
+          <Form.Label>EXPIRATION DATE (MM/YY)</Form.Label>
+          <Form.Control type="text" placeholder="MM" />
+          <Form.Control type="text" placeholder="YY" />
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formCVC">
-        <Form.Label>CVC</Form.Label>
-        <Form.Control type="text" placeholder="000" />
-      </Form.Group>
+        <Form.Group className="mb-3" controlId="formCVC">
+          <Form.Label>CVC</Form.Label>
+          <Form.Control type="text" placeholder="e.g. 123" />
+        </Form.Group>
 
-      <button className="form__btn" type="submit">
-        Submit
-      </button>
-    </Form>
+        <button className="form__btn" type="submit">
+          Submit
+        </button>
+      </Form>
+    </Container>
   );
 };
 
