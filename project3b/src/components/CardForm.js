7ccee75f4a9aafeb9687e300d1f971d2dd5d20be
brunomaria71 <brunomaria71@yahoo.com>
2023-06-27 15:53:37 -0400

    form ip

--- project3b/src/components/CardForm.js
+++ project3b/src/components/CardForm.js
@@ -5,21 +5,27 @@ import "../styles.scss";
 const CardForm = () => {
   return (
     <Form className="form">
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
+      <Form.Group className="mb-3" controlId="formCardholderName">
+        <Form.Label>CARDHOLDER NAME</Form.Label>
+        <Form.Control type="text" placeholder="Enter first and last name" />
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+      <Form.Group className="mb-3" controlId="formCardholderName">
+        <Form.Label>CARD NUMBER</Form.Label>
+        <Form.Control type="text" placeholder="0000 0000 0000 0000" />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
+
+      <Form.Group className="mb-3" controlId="formExpirationDate">
+        <Form.Label>EXPIRATION DATE (MM/YY)</Form.Label>
+        <Form.Control type="text" placeholder="MM" />
+        <Form.Control type="text" placeholder="YY" />
       </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formCVC">
+        <Form.Label>CVC</Form.Label>
+        <Form.Control type="text" placeholder="000" />
+      </Form.Group>
+
       <button className="form__btn" type="submit">
         Submit
       </button>
