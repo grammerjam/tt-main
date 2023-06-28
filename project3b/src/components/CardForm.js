87035b957e60d2b25583080c60a4d8a54dd20d83
brunomaria71 <brunomaria71@yahoo.com>
2023-06-27 22:11:10 -0400

    adding styling to form

--- project3b/src/components/CardForm.js
+++ project3b/src/components/CardForm.js
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
+import Stack from "react-bootstrap/Stack";
 import "../styles.scss";
 
 const CardForm = () => {
@@ -53,49 +54,51 @@ const CardForm = () => {
           </Form.Control.Feedback>
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formExpirationDate">
-          <Form.Label>EXPIRATION DATE (MM/YY)</Form.Label>
-          <div className="form__expiration-container">
-            <div>
-              <Form.Control
-                type="text"
-                placeholder="MM"
-                required
-                minLength={2}
-                maxLength={2}
-              />
-              <Form.Control.Feedback type="invalid">
-                Required month should be valid 01-12
-              </Form.Control.Feedback>
+        <Stack direction="horizontal" gap={3}>
+          <Form.Group className="mb-3" controlId="formExpirationDate">
+            <Form.Label>EXPIRATION DATE (MM/YY)</Form.Label>
+            <div className="form__expiration-container">
+              <div>
+                <Form.Control
+                  type="text"
+                  placeholder="MM"
+                  required
+                  minLength={2}
+                  maxLength={2}
+                />
+                <Form.Control.Feedback type="invalid">
+                  Required month should be valid 01-12
+                </Form.Control.Feedback>
+              </div>
+              <div>
+                <Form.Control
+                  type="text"
+                  placeholder="YY"
+                  required
+                  minLength={2}
+                  maxLength={2}
+                />
+                <Form.Control.Feedback type="invalid">
+                  Required year should not be before current year.
+                </Form.Control.Feedback>
+              </div>
             </div>
-            <div>
-              <Form.Control
-                type="text"
-                placeholder="YY"
-                required
-                minLength={2}
-                maxLength={2}
-              />
-              <Form.Control.Feedback type="invalid">
-                Required year should not be before current year.
-              </Form.Control.Feedback>
-            </div>
-          </div>
-        </Form.Group>
+          </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formCVC">
-          <Form.Label>CVC</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="e.g. 123"
-            required
-            minLength={3}
-            maxLength={3}
-          />
-          <Form.Control.Feedback type="invalid">
-            Must be 3 numbers.
-          </Form.Control.Feedback>
-        </Form.Group>
+          <Form.Group className="mb-3" controlId="formCVC">
+            <Form.Label>CVC</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="e.g. 123"
+              required
+              minLength={3}
+              maxLength={3}
+            />
+            <Form.Control.Feedback type="invalid">
+              Must be 3 numbers.
+            </Form.Control.Feedback>
+          </Form.Group>
+        </Stack>
 
         <button className="form__btn" type="submit">
           Submit
