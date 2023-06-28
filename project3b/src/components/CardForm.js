05e11016278fe1f312d29461e8903f0a41ceeb36
brunomaria71 <brunomaria71@yahoo.com>
2023-06-27 21:17:41 -0400

    adding validators

--- project3b/src/components/CardForm.js
+++ project3b/src/components/CardForm.js
@@ -1,16 +1,31 @@
 // import Button from "react-bootstrap/Button";
+import { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import "../styles.scss";
 
 const CardForm = () => {
-  const resetForm = () => {
+  const [validated, setValidated] = useState(false);
+
+  const resetForm = (e) => {
     window.alert("hello");
+    const form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+
+    setValidated(true);
   };
 
   return (
     <Container>
-      <Form className="form" novalidate>
+      <Form
+        className="form"
+        noValidate
+        validated={validated}
+        onSubmit={resetForm}
+      >
         <Form.Group className="mb-3" controlId="formCardholderName">
           <Form.Label>CARDHOLDER NAME</Form.Label>
           <Form.Control
@@ -18,6 +33,9 @@ const CardForm = () => {
             placeholder="e.g. Jane Appleseed"
             required
           />
+          <Form.Control.Feedback type="invalid">
+            Required first and last name.
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formCardholderName">
@@ -27,6 +45,9 @@ const CardForm = () => {
             placeholder="e.g. 1234 5678 9123 0000"
             required
           />
+          <Form.Control.Feedback type="invalid">
+            Required 16 digit card number.
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formExpirationDate">
@@ -35,14 +56,21 @@ const CardForm = () => {
             <Form.Control type="text" placeholder="MM" required />
             <Form.Control type="text" placeholder="YY" required />
           </div>
+          <Form.Control.Feedback type="invalid">
+            Required month should be a valid month and year should not be
+            expired.
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formCVC">
           <Form.Label>CVC</Form.Label>
           <Form.Control type="text" placeholder="e.g. 123" required />
+          <Form.Control.Feedback type="invalid">
+            Must be 3 numbers.
+          </Form.Control.Feedback>
         </Form.Group>
 
-        <button className="form__btn" type="submit" onClick={resetForm}>
+        <button className="form__btn" type="submit">
           Submit
         </button>
       </Form>
