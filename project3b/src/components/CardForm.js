69175a7783829d7c09252c432d1af8147c9a8189
brunomaria71 <brunomaria71@yahoo.com>
2023-06-28 17:33:13 -0400

    adding formik dependencies

--- project3b/src/components/CardForm.js
+++ project3b/src/components/CardForm.js
@@ -3,8 +3,20 @@ import { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Stack from "react-bootstrap/Stack";
+import * as Yup from "yup";
 import "../styles.scss";
 import ThankYouMessage from "./ThankYouMessage";
+
+const FormSchema = Yup.object().shape({
+  cardholderName: Yup.string().min(2).max(50, "Too Long!").required("Required"),
+  cardNumber: Yup.string()
+    .min(16, "Too Short!")
+    .max(16, "Too Long!")
+    .required("Required"),
+  expirationMonth: Yup.string().min(2).max(2).required("Required"),
+  expirationYear: Yup.string().min(2).max(2).required("Required"),
+  cvc: Yup.string().min(3).max(3).required("Required"),
+});
 
 const CardForm = () => {
   const [validated, setValidated] = useState(false);
@@ -23,94 +35,92 @@ const CardForm = () => {
 
   return (
     <>
-      {!submitForm && (
-        <Container className="form-container">
-          <Form
-            className="form"
-            noValidate
-            validated={validated}
-            onSubmit={showSuccess}
-          >
-            <Form.Group className="mb-3" controlId="formCardholderName">
-              <Form.Label>CARDHOLDER NAME</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="e.g. Jane Appleseed"
-                required
-                minLength={2}
-              />
-              <Form.Control.Feedback type="invalid">
-                Required first and last name.
-              </Form.Control.Feedback>
-            </Form.Group>
+      <Container className="form-container">
+        <Form
+          className="form"
+          noValidate
+          validated={validated}
+          onSubmit={showSuccess}
+        >
+          <Form.Group className="mb-3" controlId="formCardholderName">
+            <Form.Label>CARDHOLDER NAME</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="e.g. Jane Appleseed"
+              required
+              minLength={2}
+            />
+            <Form.Control.Feedback type="invalid">
+              Required first and last name.
+            </Form.Control.Feedback>
+          </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formCardholderName">
-              <Form.Label>CARD NUMBER</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="e.g. 1234 5678 9123 0000"
-                required
-                minLength={16}
-                maxLength={16}
-              />
-              <Form.Control.Feedback type="invalid">
-                Required 16 digit card number.
-              </Form.Control.Feedback>
-            </Form.Group>
+          <Form.Group className="mb-3" controlId="formCardholderName">
+            <Form.Label>CARD NUMBER</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="e.g. 1234 5678 9123 0000"
+              required
+              minLength={16}
+              maxLength={16}
+            />
+            <Form.Control.Feedback type="invalid">
+              Required 16 digit card number.
+            </Form.Control.Feedback>
+          </Form.Group>
 
-            <Stack direction="horizontal" gap={3}>
-              <Form.Group className="mb-3" controlId="formExpirationDate">
-                <Form.Label>EXPIRATION DATE (MM/YY)</Form.Label>
-                <div className="form__expiration-container">
-                  <div>
-                    <Form.Control
-                      type="text"
-                      placeholder="MM"
-                      required
-                      minLength={2}
-                      maxLength={2}
-                    />
-                    <Form.Control.Feedback type="invalid">
-                      Required month should be valid 01-12
-                    </Form.Control.Feedback>
-                  </div>
-                  <div>
-                    <Form.Control
-                      type="text"
-                      placeholder="YY"
-                      required
-                      minLength={2}
-                      maxLength={2}
-                    />
-                    <Form.Control.Feedback type="invalid">
-                      Required year should not be before current year.
-                    </Form.Control.Feedback>
-                  </div>
+          <Stack direction="horizontal" gap={3}>
+            <Form.Group className="mb-3" controlId="formExpirationDate">
+              <Form.Label>EXPIRATION DATE (MM/YY)</Form.Label>
+              <div className="form__expiration-container">
+                <div>
+                  <Form.Control
+                    type="text"
+                    placeholder="MM"
+                    required
+                    minLength={2}
+                    maxLength={2}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    Required month should be valid 01-12
+                  </Form.Control.Feedback>
                 </div>
-              </Form.Group>
+                <div>
+                  <Form.Control
+                    type="text"
+                    placeholder="YY"
+                    required
+                    minLength={2}
+                    maxLength={2}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    Required year should not be before current year.
+                  </Form.Control.Feedback>
+                </div>
+              </div>
+            </Form.Group>
 
-              <Form.Group className="mb-3" controlId="formCVC">
-                <Form.Label>CVC</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="e.g. 123"
-                  required
-                  minLength={3}
-                  maxLength={3}
-                />
-                <Form.Control.Feedback type="invalid">
-                  Must be 3 numbers.
-                </Form.Control.Feedback>
-              </Form.Group>
-            </Stack>
+            <Form.Group className="mb-3" controlId="formCVC">
+              <Form.Label>CVC</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="e.g. 123"
+                required
+                minLength={3}
+                maxLength={3}
+              />
+              <Form.Control.Feedback type="invalid">
+                Must be 3 numbers.
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Stack>
 
-            <button className="form__btn" type="submit">
-              Submit
-            </button>
-          </Form>
-        </Container>
-      )}
-      {submitForm && validated && <ThankYouMessage />}
+          <button className="form__btn" type="submit">
+            Submit
+          </button>
+        </Form>
+      </Container>
+      <ThankYouMessage />
     </>
   );
 };
